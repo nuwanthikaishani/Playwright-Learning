ba664fefecae986f0be0a2eae36ae7d59f1bfc69
Ishani Nuwanthika(QA Journal by Ish) <nuwanthikaishani@gmail.com>
2025-01-19 17:24:57 +0530

    auth file

---
+++ tests/auth.setup.js
@@ -0,0 +1,7 @@
+import {test as setup, expect} from "@playwright/test";
+
+setup("create customer 01 auth", async ({ page , context }) => {
+    const email = "customer@practicesoftwaretesting.com";
+    const password ="welcome01";
+    const customer01AuthFile = ".auth/customer01.json";
+})
