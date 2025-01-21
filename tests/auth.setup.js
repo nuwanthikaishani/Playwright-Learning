f13c4999c389f70702cdddc38698bddcd9d04ec4
Ishani Nuwanthika(QA Journal by Ish) <nuwanthikaishani@gmail.com>
2025-01-21 16:41:47 +0530

    Playwright auth file added

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -4,4 +4,7 @@ setup("create customer 01 auth", async ({ page , context }) => {
     const email = "customer@practicesoftwaretesting.com";
     const password ="welcome01";
     const customer01AuthFile = ".auth/customer01.json";
+
+    await page.goto("https://practicesoftwaretesting.com/auth/login");
+    
 })
